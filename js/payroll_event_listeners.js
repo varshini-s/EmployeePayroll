15a208e1e8a3efa53e5884b8688288fc91cb7252
varshini <dreamvarshini@gmail.com>
2021-10-12 19:06:14 +0530

    ADDED: function to store employee details in local storage

--- js/payroll_event_listeners.js
+++ js/payroll_event_listeners.js
@@ -53,13 +53,31 @@ const save=()=>{
     try {
 
         let employeePayrollData= createEmployeePayroll();
+        createAndUpdateStorage(employeePayrollData);
         
     }
-     catch (error) 
+     catch (e) 
     {
         return;
         
     }
+}
+
+function createAndUpdateStorage(employeePayrollData)
+{
+    let employeePayrollList=JSON.parse(localStorage.getItem("EmployeePayrollList"));
+    
+    if(employeePayrollList!=undefined)
+    {
+        employeePayrollList.push(employeePayrollData);
+    }
+    else
+    {
+        employeePayrollList=[employeePayrollData]
+    }
+
+    alert(employeePayrollList.toString());
+    localStorage.setItem("EmployeePayrollList",JSON.stringify(employeePayrollList));
 }
 
 const createEmployeePayroll=()=>{
