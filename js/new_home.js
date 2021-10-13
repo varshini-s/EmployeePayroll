da79b991d3e03fe29d93e8271d4fd8e81a89ab3f
varshini <dreamvarshini@gmail.com>
2021-10-13 21:05:47 +0530

    ADDED: Function to create employee payroll JSON

--- js/new_home.js
+++ js/new_home.js
@@ -7,24 +7,56 @@ const createInnerHtml=()=>{
     const headerHTML ="<th></th> <th>Name</th> <th>Gender</th>"+
                      "<th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th>";
 
+    let empPayrollData=createEmployeePayrollJSON()[1];
     const innerHtml=`${headerHTML}
  
     <tr>
-        <td><img class="profile" alt="" src="../assets/profile-images/Ellipse -2.png">
-        </td>
-        <td>Narayan Mahadevan</td>
-        <td>Male</td>
-        <td><div class="dept-label">HR</div>
-            <div class="dept-label">Finance</div></td>
-        <td>3000000</td>
-        <td>1 Nov 2020</td>
+        <td><img class="profile" alt="" src="${empPayrollData._profilePic}"></td>
+        <td>${empPayrollData._name}</td>
+        <td>${empPayrollData._gender}</td>
+        <td><div class="dept-label">${empPayrollData._department[0]}</div>
+            <div class="dept-label">${empPayrollData._department[1]}</div></td>
+        <td>${empPayrollData._salary}</td>
+        <td>${empPayrollData._startDate}</td>
         <td>
-            <img id="1" onclick="remove(this)" alt="delete"
+            <img name="$empPayrollData._id}" onclick="remove(this)" alt="delete"
                     src="../assets/icons/delete-black-18dp.svg">
-            <img id="1" alt="edit" onclick="update(this)"
+            <img name="$empPayrollData._id}" alt="edit" onclick="update(this)"
                     src="../assets/icons/create-black-18dp.svg">
         </td>
     </tr>
     `;
     document.querySelector('#table-display').innerHTML=innerHtml;
+}
+
+const createEmployeePayrollJSON=()=>{
+    let empPayrollListLocal=[
+        {
+            _name:'Narayan Mahadevan',
+            _gender:'male',
+            _department:[
+                'Engineering',
+                'Finance'
+            ],
+            _salary:'500000',
+            _startDate:'29 Oct 2019',
+            _note:'',
+            _id:new Date().getTime(),
+            _profilePic:'../assets/profile-images/Ellipse -2.png'
+        },
+        {
+            _name:'Amarpa Shashanka keerthi kumar',
+            _gender:'male',
+            _department:[
+                'Sales'
+            ],
+            _salary:'400000',
+            _startDate:'29 Oct 2019',
+            _note:'',
+            _id:new Date().getTime()+1,
+            _profilePic:'../assets/profile-images/Ellipse -1.png'
+
+        }
+    ];
+    return empPayrollListLocal;
 }
