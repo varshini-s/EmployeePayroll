5e2ca19da20a7339768b86be87c30d72278625ed
varshini <dreamvarshini@gmail.com>
2021-10-15 11:30:15 +0530

    ADDED: function to update employee payroll data

--- js/new_home.js
+++ js/new_home.js
@@ -68,3 +68,9 @@ const remove = (node) => {
     createInnerHtml();
 }
 
+const update=(node)=>{
+    let empPayrollData=empPayrollList.find(empData=>empData._id==node.id)
+    if (!empPayrollData) return;
+    localStorage.setItem('editEmp',JSON.stringify(empPayrollData))
+    window.location.replace(site_properties.add_emp_payroll_page);
+}
