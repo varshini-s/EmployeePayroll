16ddfa74e6c44178714c12f64466ba75e901ac6e
varshini <dreamvarshini@gmail.com>
2021-10-15 16:00:14 +0530

    ADDED: function to create employee payroll data

--- js/payroll_event_listeners.js
+++ js/payroll_event_listeners.js
@@ -124,6 +124,44 @@ function createAndUpdateStorage()
     localStorage.setItem("EmployeePayrollList",JSON.stringify(employeePayrollList));
 }
 
+const createEmployeePayrollData=(id)=>{
+
+    let employeePayrollData = new EmployeePayrollData();
+    if(!id) employeePayrollData._id= createNewEmployeeId();
+    else employeePayrollData._id=id;
+    setEmployeePayrollData(employeePayrollData);
+    return employeePayrollData;
+}
+
+const setEmployeePayrollData=(employeePayrollData)=>{
+    try
+    {
+        employeePayrollData.name=employeePayrollObj._name;
+    }
+    catch(e)
+    {
+        setTextValue('.text-error',e);
+        throw e;
+    }
+    employeePayrollData.profilePic =employeePayrollObj._profilePic;
+    employeePayrollData.gender = employeePayrollObj._gender;
+    employeePayrollData.department = employeePayrollObj._department;
+    employeePayrollData.salary = employeePayrollObj._salary;
+    employeePayrollData.note = employeePayrollObj._note;
+
+    try 
+    {
+        employeePayrollData.startDate=new Date(Date.parse(employeePayrollObj._startDate));
+        
+    } catch (e) 
+    {
+        setTextValue('.date-error',e);
+        throw e;
+    }
+
+    alert(employeePayrollData.toString());
+}
+
 const createEmployeePayroll=()=>{
 
     let employeePayrollList=JSON.parse(localStorage.getItem("EmployeePayrollList"));
