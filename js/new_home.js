5e2a527c1a67a4ba8d829f6f6b226b3a2a5f2f7e
varshini <dreamvarshini@gmail.com>
2021-10-18 15:38:47 +0530

    ADDED: function to make service call

--- js/new_home.js
+++ js/new_home.js
@@ -122,8 +122,50 @@ const remove = (node) => {
 }
 
 const update=(node)=>{
-    let empPayrollData=empPayrollList.find(empData=>empData._id==node.id)
+    let empPayrollData=empPayrollList.find(empData=>empData.id==node.id)
     if (!empPayrollData) return;
     localStorage.setItem('editEmp',JSON.stringify(empPayrollData))
     window.location.replace(site_properties.add_emp_payroll_page);
+}
+
+function makeServiceCall(methodType,url,async=true,data=null)
+{
+    return new Promise(function(resolve,request)
+    {
+        let xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function(){
+            console.log(methodType+" state changed called Ready state: "+xhr.readyState+" Status: "+xhr.status)
+            if(xhr.readyState==4)
+            {
+                if(xhr.status.toString().match('^[2][0-9]{2}$'))
+                {
+                    resolve(xhr.responseText)
+                }
+                else if(xhr.status.toString().match('^[4,5][0-9]{2}$'))
+                {
+                    reject({
+                        status:xhr.status,
+                        statusText:xhr.statusText
+                    });
+                    console.log("XHR failed");
+                }
+
+            }
+            
+        }
+        xhr.open(methodType,url,async);
+
+        if(data)
+        {
+            console.log(JSON.stringify(data));
+            xhr.setRequestHeader("Content-Type","application/json");
+            xhr.send(JSON.stringify(data));
+        }
+        else
+        {
+            xhr.send();
+        }
+        console.log(methodType+" Request sent to server ")
+    
+    });
 }
