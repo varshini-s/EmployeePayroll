a4c55f94fadf53fda6dcc14d5aeb1b2bbf23e5f4
varshini <dreamvarshini@gmail.com>
2021-10-18 15:30:13 +0530

    ADDED: function to save data into server

--- js/payroll_event_listeners.js
+++ js/payroll_event_listeners.js
@@ -98,6 +98,29 @@ const save = (event) => {
 }
 
 
+const createOrUpdateEmployeePayroll=()=>{
+
+    let postURL = site_properties.server_url;
+    let methodCall="POST";
+    if(isUpdate)
+    {
+        methodCall="PUT";
+        postURL=postURL+employeePayrollObj.id.toString();
+    }
+
+    makeServiceCall(methodCall,postURL,true,employeePayrollObj)
+    .then(responseText=>{
+        resetForm();
+        window.location.replace(site_properties.home_page);
+    })
+    .catch(error=>{
+        throw error;
+    })
+
+
+}
+
+
 const setEmployeePayrollObject = () => {
     employeePayrollObj._name = getInputValueById('#name');
     employeePayrollObj._profilePic = getSelctedValues('[name=profile]').pop();
